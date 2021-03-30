45d9a8cb4b62892e6381fda2a989a9461a170078
abdykapar <abdykaparkamilov@gmail.com>
2021-03-30 12:51:51 +0500

    remove: category unique title

--- models/category.js
+++ models/category.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose')
 const categorySchema = new mongoose.Schema({
     title: {
         type: String,
-        required: true,
-        unique: true
+        required: true
     },
     parent: {
         type: String,
