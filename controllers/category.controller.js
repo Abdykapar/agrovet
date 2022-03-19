5255d2e1ad711fddf795faa160aea8b6c0907b8d
Abdykapar Kamilov <abdykaparkamilov@mail.ru>
2022-03-19 14:58:34 +0600

    add query to catefories and products pai

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -1,4 +1,22 @@
 const controller = require('./generateControllers')
 const Category = require('../models/category')
 
-module.exports = controller.generateControllers(Category)
+module.exports = controller.generateControllers(Category, {
+    getChild: async (req, res, next) => {
+        try {
+            let q = {};
+            if(req.query.categoryId) {
+              q.parent = req.query.categoryId
+            } else{
+                q.parent = { $ne: '' }
+            }
+            const items = await Category.find(q).populate('parent')
+            return res.status(200).json(items)
+          } catch(err) {
+            if (!err.statusCode) {
+              err.statusCode = 500
+            }
+            next(err)
+          }
+    }
+})
--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -4,4 +4,19 @@ const Product = require('../models/product')
 module.exports = controller.generateControllers(Product, {
   nested: 'category',
   isImages: true,
+  getAllWithPopulate: async (req, res, next) => {
+    try {
+      let q = {};
+      if(req.query.categoryId) {
+        q.category = req.query.categoryId
+      }
+      const items = await Product.find(q).populate('category')
+      return res.status(200).json(items)
+    } catch(err) {
+      if (!err.statusCode) {
+        err.statusCode = 500
+      }
+      next(err)
+    }
+  },
 })
